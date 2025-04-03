9d0da9bb8de483af75a2b1c8eb586f0cf060eef2
talal.attar <talal.attar@ingka.ikea.com>
2025-04-03 15:43:20 +0200

    chore: save more data of cookie

--- server/models/Consent.js
+++ server/models/Consent.js
@@ -11,6 +11,19 @@ const consentSchema = new mongoose.Schema(
       type: Date,
       default: Date.now, // Automatically set the timestamp when the consent is recorded
     },
+    language: {
+      type: String, // Store the user's language (e.g., "en", "ar")
+      required: false,
+    },
+    userAgent: {
+      type: String, // Store the user's browser or device information
+      required: false,
+    },
+    consentTypes: {
+      type: Map, // Store detailed consent types (e.g., essential, analytics, marketing)
+      of: Boolean,
+      required: false,
+    },
   },
   { timestamps: true }
 );
